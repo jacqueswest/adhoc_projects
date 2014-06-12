b90e258c9ac0673c293c761e63f6740c352ec075
Jacques Van Der Westhuizen <jacqueswest@gmail.com>
2014-06-12 23:11:48 +0200

    creating a list that only contains the requested info

--- youtube_vids/get_url.py
+++ youtube_vids/get_url.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python
 
-from json import loads
+from json import loads, dumps
 import requests
 
-urls = requests.get("http://gdata.youtube.com/feeds/api/standardfeeds/top_rated?v=2&alt=jsonc")
-print loads(urls.text)
+top_rated = "http://gdata.youtube.com/feeds/api/standardfeeds/top_rated?v=2&alt=jsonc"
+
+def get_youtube_urls(search_url):
+    ''' Returns a list of top rated youtube urls in json format '''
+    urls = requests.get(search_url)
+    if urls.status_code != 200:
+        print "No results found with error code %s" % urls.status_code
+        sys.exit(-1)
+    return urls.json()
+
+def videos_list(data):
+    ''' return a list of urls '''
+    videos_dict = {"videos":{}}
+    for item in data['data']['items']:
+        videos_dict['videos'].update({item['id']:{}})
+        videos_dict['videos'][item['id']].update({'title': item['title']})
+        videos_dict['videos'][item['id']].update({'url': item['player']['default']})
+    return dumps(videos_dict)
+
+data = get_youtube_urls(top_rated)
+
+videos_dict = {"videos":{}}
+print videos_list(data)
+
+
